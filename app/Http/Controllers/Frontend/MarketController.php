82720ba2b453547dfdd2c83c7e256af745aed250
Vento <vento@pfadi-nuenenen.ch>
2021-10-20 22:34:31 +0200

    Create MarketController.php

---
+++ app/Http/Controllers/Frontend/MarketController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+
+class MarketController extends Controller
+{
+    public function index()
+    {
+        return view();
+    }
+}
