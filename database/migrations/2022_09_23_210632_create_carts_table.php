88c12103f7227eca96f64a750825425bcde737eb
Vento <github@brenneisen.me>
2022-11-05 21:48:49 +0100

    Update 2022_09_23_210632_create_carts_table.php

--- database/migrations/2022_09_23_210632_create_carts_table.php
+++ database/migrations/2022_09_23_210632_create_carts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('item_variation_id');.
+            $table->foreignId('item_variation_id');
             $table->integer('quantity');
             $table->foreignId('customer_id');
             $table->dateTime('start_date');
