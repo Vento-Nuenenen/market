90f7923ab0de6e38dac5dd01ece66a3ee37c804e
Vento <github@brenneisen.me>
2022-11-05 21:48:40 +0100

    Update Color.php

--- app/Models/Color.php
+++ app/Models/Color.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Color extends Model
 {
     use HasFactory;
+
+    protected $table = 'colors';
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'hex_code'
+    ];
 }
