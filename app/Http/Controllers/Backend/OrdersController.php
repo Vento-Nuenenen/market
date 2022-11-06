87cbe16557cb193fc05c0ee272b891bd681fe2eb
Vento <github@brenneisen.me>
2022-11-06 14:04:10 +0100

    Create OrdersController.php

---
+++ app/Http/Controllers/Backend/OrdersController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+
+class OrdersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Request $request
+     *
+     * @return Application|Factory|View
+     */
+    public function index(Request $request)
+    {
+
+    }
+}
