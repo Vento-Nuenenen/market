68466eb08128956766f29f2d5689f8b7ba1a0028
Vento <github@brenneisen.me>
2022-11-07 19:40:14 +0100

    Update bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,6 +3,6 @@ window._ = _;
 
 import 'bootstrap';
 
-import axios from "axios";
+import axios from 'axios';
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
