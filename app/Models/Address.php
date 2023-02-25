eeb3cbb717a4922edd42b94b980832caf9b43fe3
Vento <github@brenneisen.me>
2023-02-25 20:51:36 +0100

    Update Address.php

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -8,10 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class Address extends Model
 {
     use HasFactory;
-
-    protected $table = 'addresses';
-
-    protected $fillable = [
-        '',
-    ];
 }
