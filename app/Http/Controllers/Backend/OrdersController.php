74626d030d0425a0d72219efa66368131cf60848
Vento <github@brenneisen.me>
2023-02-25 20:57:39 +0100

    Update OrdersController.php

--- app/Http/Controllers/Backend/OrdersController.php
+++ app/Http/Controllers/Backend/OrdersController.php
@@ -3,22 +3,16 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Contracts\Foundation\Application;
-use Illuminate\Contracts\View\Factory;
-use Illuminate\Contracts\View\View;
-use Illuminate\Http\Request;
 
 class OrdersController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @param Request $request
      *
      * @return Application|Factory|View
      */
     public function index(Request $request)
     {
-
     }
 }
